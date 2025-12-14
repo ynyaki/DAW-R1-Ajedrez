2b2d72fbf60714bece2dfec5f48b32f7ed1d9088
Juan <juarodfer3@alu.edu.gva.es>
2025-12-14 12:44:37 +0100

    Creado metodo para imprimir/visualizar el Tablero.java.

--- src/Ajedrez/Tablero.java
+++ src/Ajedrez/Tablero.java
@@ -14,17 +14,8 @@ public class Tablero {
 
     // Colores
     public static final String NEGRO = "\u001B[40m";
-    public static final String ROJO = "\u001B[41m";
-    public static final String VERDE = "\u001B[42m";
-    public static final String AMARILLO = "\u001B[43m";
-    public static final String AZUL = "\u001B[44m";
-    public static final String CIAN = "\u001B[46m";
     public static final String BLANCO = "\u001B[47m";
     public static final String RESET = "\u001B[0m";
-
-    public Tablero(){
-
-    }
 
     public void print() {
         // TODO Recoger el simbolo de la pieza.
