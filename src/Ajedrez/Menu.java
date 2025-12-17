7ac3e476ceb7fcf2ebe235f7b9854a939931aced
ynyaki <inaki.bazan.974@gmail.com>
2025-12-17 09:15:43 +0100

    [17/12] Unión de Tablero, pruebas para Menú, crear lógica básica para Partida.

--- src/Ajedrez/Menu.java
+++ src/Ajedrez/Menu.java
@@ -4,13 +4,11 @@ public abstract class Menu {
 
     private static Scanner sc;
 
-    // TODO Desarrollar menú
     public static void ejecutar(Scanner sc) {
         Menu.sc = sc;
 
-        //  DELETE Pruebas de creación y muestra de tablero
-        Tablero t = new Tablero(8, 8);
-        t.colocar(new Pieza(Pieza.Tipo.TORRE, Pieza.Color.BLANCO, new Posicion(1, 1)));
-        System.out.println(t);
+        // DELETE Pruebas de Partida
+        Partida p = new Partida();
+        p.empezar();
     }
 }
