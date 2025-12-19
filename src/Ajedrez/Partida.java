a731247c6a0ef7ae66bf403c519fa17b55b85be3
ynyaki <inaki.bazan.974@gmail.com>
2025-12-19 14:13:44 +0100

    Creada lógica del jaque [19/12]

--- src/Ajedrez/Partida.java
+++ src/Ajedrez/Partida.java
@@ -11,18 +11,17 @@ public class Partida {
     private static final Pieza.Color NEGRO = Pieza.Color.NEGRO;
 
     private Tablero t;
+    private Pieza rB;
+    private Pieza rN;
 
+    // DELETE Pruebas de la clase
     public static void main() {
-        System.out.println("PRUEBAS DE PARTIDA");
-        System.out.println("------------------");
-        System.out.println();
         Partida p = new Partida();
         p.empezar();
     }
 
-    //  DELETE Pruebas de creación y muestra de tablero
     public void empezar() {
-        Tablero t = crearTableroClasico();
+        t = crearTableroClasico();
         Pieza p1 = new Pieza(TORRE, BLANCO, 1, 8);
         colocar(p1);
         System.out.print(t);
@@ -31,7 +30,12 @@ public class Partida {
     public void colocar(Pieza p) {
         if(esColocacionValida(p))
             t.setPieza(p);
+        if(p.getTipo() == REY) {
+            // TODO Asignar pieza a rB y rN
+        }
     }
+
+    // Métodos de validación
 
     public boolean esColocacionValida(Pieza p) {
         return piezaDentroDeLimites(p)
@@ -104,36 +108,65 @@ public class Partida {
         return posicionValida;
     }
 
+    // Métodos de movimiento
+
+    private boolean esMovLegal(Pieza p, Posicion pos) {
+        return false;
+    }
+
+    public Pieza.Color getJaque() {
+        Pieza.Color color;
+        if(hayJaque(rB))
+            color = BLANCO;
+        else if(hayJaque(rN))
+            color = NEGRO;
+        else
+            color = null;
+        return color;
+    }
+
+    private boolean hayJaque(Pieza rey) {
+        boolean hayJaque = false;
+        for(Pieza[] fila : t.get())
+            for(Pieza pieza : fila)
+                if(pieza.getColor() != rey.getColor()
+                        && esMovLegal(pieza, rey.getPos()) && !hayJaque)
+                    hayJaque = true;
+        return hayJaque;
+    }
+
+    // DELETE Métodos provisionales
+
     private Tablero crearTableroClasico() {
         this.t = new Tablero(8, 8);
 
-        t.setPieza(new Pieza(TORRE, BLANCO, new Posicion(1, 1)));
-        t.setPieza(new Pieza(TORRE, BLANCO, new Posicion(8, 1)));
-        t.setPieza(new Pieza(TORRE, NEGRO, new Posicion(1, 8)));
-        t.setPieza(new Pieza(TORRE, NEGRO, new Posicion(8, 8)));
-        t.setPieza(new Pieza(CABALLO, BLANCO, new Posicion(2, 1)));
-        t.setPieza(new Pieza(CABALLO, BLANCO, new Posicion(7, 1)));
-        t.setPieza(new Pieza(CABALLO, NEGRO, new Posicion(2, 8)));
-        t.setPieza(new Pieza(CABALLO, NEGRO, new Posicion(7, 8)));
+        t.setPieza(new Pieza(TORRE, BLANCO, 1, 1));
+        t.setPieza(new Pieza(TORRE, BLANCO, 8, 1));
+        t.setPieza(new Pieza(TORRE, NEGRO, 1, 8));
+        t.setPieza(new Pieza(TORRE, NEGRO, 8, 8));
+        t.setPieza(new Pieza(CABALLO, BLANCO, 2, 1));
+        t.setPieza(new Pieza(CABALLO, BLANCO, 7, 1));
+        t.setPieza(new Pieza(CABALLO, NEGRO, 2, 8));
+        t.setPieza(new Pieza(CABALLO, NEGRO, 7, 8));
 
-        t.setPieza(new Pieza(ALFIL, BLANCO, new Posicion(3, 1)));
-        t.setPieza(new Pieza(ALFIL, BLANCO, new Posicion(6, 1)));
-        t.setPieza(new Pieza(ALFIL, NEGRO, new Posicion(3, 8)));
-        t.setPieza(new Pieza(ALFIL, NEGRO, new Posicion(6, 8)));
+        t.setPieza(new Pieza(ALFIL, BLANCO, 3, 1));
+        t.setPieza(new Pieza(ALFIL, BLANCO, 6, 1));
+        t.setPieza(new Pieza(ALFIL, NEGRO, 3, 8));
+        t.setPieza(new Pieza(ALFIL, NEGRO, 6, 8));
 
-        t.setPieza(new Pieza(DAMA, BLANCO, new Posicion(4, 1)));
-        t.setPieza(new Pieza(DAMA, NEGRO, new Posicion(4, 8)));
+        t.setPieza(new Pieza(DAMA, BLANCO, 4, 1));
+        t.setPieza(new Pieza(DAMA, NEGRO, 4, 8));
 
-        t.setPieza(new Pieza(REY, BLANCO, new Posicion(5, 1)));
-        t.setPieza(new Pieza(REY, NEGRO, new Posicion(5, 8)));
-
-        for(int i = 1; i <= 8; i++)
-            t.setPieza(new Pieza(PEON, BLANCO, new Posicion(i, 2)));
+        t.setPieza(new Pieza(REY, BLANCO, 5, 1));
+        t.setPieza(new Pieza(REY, NEGRO, 5, 8));
 
         for(int i = 1; i <= 8; i++)
-            t.setPieza(new Pieza(PEON, NEGRO, new Posicion(i, 7)));
+            t.setPieza(new Pieza(PEON, BLANCO, i, 2));
 
-        t.setPieza(new Pieza(PEON, NEGRO, new Posicion(8, 8)));
+        for(int i = 1; i <= 8; i++)
+            t.setPieza(new Pieza(PEON, NEGRO, i, 7));
+
+        t.setPieza(new Pieza(PEON, NEGRO, 8, 8));
 
         return t;
     }
