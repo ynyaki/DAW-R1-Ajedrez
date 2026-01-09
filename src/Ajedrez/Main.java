13ee217b3f3b71d34daf712c245b899c8d918b09
Ivan <ivaye2@alu.edu.gva.es>
2026-01-09 09:17:39 +0100

    conflicto resuelto

--- src/Ajedrez/Main.java
+++ src/Ajedrez/Main.java
@@ -4,18 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        try {
-            String pos = sc.nextLine();
-            sc.close();
-        }
-        catch (StringIndexOutOfBoundsException e){
-            if(e.getMessage()==null)
-                System.out.println("Error!");
-            else
-                System.out.println(e.getMessage());
-        }
-        catch (NumberFormatException e) {
-            System.out.println("Error de formato!");
-        }
+        Menu.ejecutar(sc);
+        sc.close();
     }
 }
